268d36a247175c2527651b092921c2e12f348cf3
Eldhose Kuriakose <56919546+EldhoseKuriakose@users.noreply.github.com>
2020-06-01 14:17:51 +0530

    Add increase quantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,6 +9,10 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity= () => {
+        console.log('this.state', this.state);
     }
     render() {
         const{price, title, qty} = this.state;
@@ -23,9 +27,22 @@ class CartItem extends React.Component {
                     <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828926.svg" />
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828906.svg" />
-                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+                        <img
+                         alt="increase" 
+                         className="action-icons" 
+                         src="https://image.flaticon.com/icons/svg/1828/1828926.svg"
+                         onClick={this.increaseQuantity.bind(this)}
+                        />
+                        <img
+                         alt="decrease" 
+                         className="action-icons" 
+                         src="https://image.flaticon.com/icons/svg/1828/1828906.svg" 
+                        />
+                        <img
+                         alt="delete" 
+                         className="action-icons" 
+                         src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+                        />
                     </div>
                 </div>
             </div>
